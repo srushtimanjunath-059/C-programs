252481ca6a75e90538c62d5576aad13041078103
Srushti Manjunath <srushtimanjunath059@gmail.com>
2026-02-19 10:00:34 +0530

    Add Fibonacci series implementation in C

---
+++ Fibonacci series.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main ()
+{
+    int n,first=0,second=1,next,i;
+    printf("enter the number:");
+    scanf("%d",&n);
+    for(int i=0;i<n;i++)
+    {
+        printf("%d",first);
+        next=first+second;
+        first=second;
+        second=next;
+    }
+}
